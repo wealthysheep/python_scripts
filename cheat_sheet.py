2ee635c3950eeb09cc54f7c8d56e0ad20004afe8
wealthysheep <sigridschneider@outlook.com>
2025-04-11 14:51:29 -0500

    practing python.

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -1,2 +1,19 @@
 # this is where i mess around with stuff.
 print("Hello, World.")
+
+# variables
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "Python Programming"
+print(students_count)
+
+message = """To Whom This May Concern,
+
+Congratulations on your new job."""
+length_of_message = len(message)
+print(length_of_message)
+print(message[10])
+print(message[0:10])
+print(message[0:])
+print(message[:9])
